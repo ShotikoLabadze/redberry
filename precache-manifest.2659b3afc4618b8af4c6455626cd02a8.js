b3fa8b768301a99d3c7e144862b0dc7a6feae55a
ShotaL <52139226+ShotikoLabadze@users.noreply.github.com>
2022-09-05 19:53:25 +0400

    Updates

---
+++ precache-manifest.2659b3afc4618b8af4c6455626cd02a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "32d63932eadc9c116344",
+    "url": "/redberry/static/css/main.66129f3d.chunk.css"
+  },
+  {
+    "revision": "32d63932eadc9c116344",
+    "url": "/redberry/static/js/main.32d63932.chunk.js"
+  },
+  {
+    "revision": "d73fce75d7b62164ab65",
+    "url": "/redberry/static/js/1.d73fce75.chunk.js"
+  },
+  {
+    "revision": "50ba566f9bbc61d978f2",
+    "url": "/redberry/static/js/runtime~main.50ba566f.js"
+  },
+  {
+    "revision": "b2d10ea3ffbfcb88fc25d9bd3cfe6c66",
+    "url": "/redberry/static/media/RedberryIMG.b2d10ea3.jpg"
+  },
+  {
+    "revision": "c965b2db764efa3013c792d103a88925",
+    "url": "/redberry/static/media/Group.c965b2db.png"
+  },
+  {
+    "revision": "130df01865a6bee70b2a0d43ba255e13",
+    "url": "/redberry/static/media/Vector.130df018.svg"
+  },
+  {
+    "revision": "e3a027e9bfe19a5f3072ccd4efa3c40f",
+    "url": "/redberry/static/media/HelveticaNeue.e3a027e9.ttc"
+  },
+  {
+    "revision": "124beca8ea0ea0e07329415e8a8c9820",
+    "url": "/redberry/index.html"
+  }
+];
