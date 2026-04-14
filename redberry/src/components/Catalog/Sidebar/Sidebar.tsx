af43b493cfee21e8aaa1b98b535ef70f7c9d0e5f
ShotaLaabb <shotikoslabadzes1@gmail.com>
2026-04-14 11:36:20 +0400

    feat: fetch categories and topics via api

--- redberry/src/components/Catalog/Sidebar/Sidebar.tsx
+++ redberry/src/components/Catalog/Sidebar/Sidebar.tsx
@@ -1,7 +1,73 @@
+import { useEffect, useState } from "react";
+import { getCategories, getTopics } from "../../../api/course.service";
+import "./Sidebar.css";
+
 const Sidebar = () => {
+  const [categories, setCategories] = useState<any[]>([]);
+  const [topics, setTopics] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      try {
+        const [catsRes, topsRes] = await Promise.all([
+          getCategories(),
+          getTopics(),
+        ]);
+        setCategories(catsRes.data);
+        setTopics(topsRes.data);
+      } catch (err) {
+        console.error("cant fetch data", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchInitialData();
+  }, []);
+
+  if (loading) return <div className="loading">loading...</div>;
+
   return (
-    <aside>
-      <h3>filters </h3>
+    <aside className="sidebar-container">
+      <div className="sidebar-header">
+        <h1 className="filters-title">Filters</h1>
+        <button className="clear-all-btn">
+          Clear All Filters
+          <img src="/CloseVector.png" alt="clear" className="vector-icon" />
+        </button>
+      </div>
+
+      <div className="sidebar-content">
+        <section className="filter-section">
+          <h3 className="section-label">Categories</h3>
+          <div className="filter-wrapper">
+            {categories.map((cat) => (
+              <button key={cat.id} className="filter-chip">
+                <div className="chip-icon">
+                  <img src={`/${cat.icon}.png`} alt={cat.name} />
+                </div>
+                <span className="chip-text">{cat.name}</span>
+              </button>
+            ))}
+          </div>
+        </section>
+
+        <section className="filter-section">
+          <h3 className="section-label">Topics</h3>
+          <div className="filter-wrapper">
+            {topics.map((topic) => (
+              <button key={topic.id} className="filter-chip no-icon">
+                <span className="chip-text">{topic.name}</span>
+              </button>
+            ))}
+          </div>
+        </section>
+      </div>
+
+      <div className="sidebar-footer">
+        <div className="footer-line"></div>
+        <span className="active-count">0 F</span>
+      </div>
     </aside>
   );
 };
